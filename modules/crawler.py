cbe7e106c8036b8417d53a3dac6639524131de33
Manh Cuong <cuongpigerr@gmail.com>
2021-09-04 11:47:19 +0700

    add comment to my code

--- modules/crawler.py
+++ modules/crawler.py
@@ -8,7 +8,7 @@ import pandas as pd
 import re
 import requests
 
-from typing import List
+from typing import List, Tuple
 
 def waitPageLoaded(pdriver, plogoClassname):
     '''
@@ -196,21 +196,49 @@ headers = {
 }
 
 def getProductReviewsAPI(pproductURL: str) -> (List[Review]):
-    def collect_comment(ppage: int, pproductID: str, pshopID: str):
-        params = (
-        ('filter', '0'),
-        ('flag', '1'),
-        ('itemid', pproductID),
-        ('limit', '6'),
-        ('offset', ppage),
-        ('shopid', pshopID),
-        ('type', '0'),
-        )
+    """
+    Hàm này dùng để crawl data bằng cách sử dụng API.
 
+    Args:
+        pproductURL (str): là URL đến với trang riêng của sản phẫm.
+    """
+    def collect_comment(ppage: int, pproductID: str, pshopID: str):
+        """
+        Một hàm bổ trợ dùng để crawl data trên một trang nhất định (navigation)
+
+        Args:
+            ppage (int): trang số mấy ta muốn lấy, bắt đầu từ 0
+            pproductID (str): mã id của sản phẩm
+            pshopID (str): mã id của người bán sản phẩm
+
+        Returns:
+            List[Review]:
+        """
+        
+        ''' Các tham số cần truyền vào API để crawl data '''
+        params = (
+            ('filter', '0'),
+            ('flag', '1'),
+            ('itemid', pproductID),
+            ('limit', '6'), # số 6 ở đây là nếu đếm số comment trong một navigation thì một trang có tối đa là 6 comment
+            ('offset', ppage),
+            ('shopid', pshopID),
+            ('type', '0'))
+
+        ''' Gửi một request đến API và nhận về response '''
         response = requests.get('https://shopee.vn/api/v2/item/get_ratings', headers=headers, params=params)
         return response.json().get('data').get('ratings')
     
-    def parse_comment(presponse):
+    def parse_comment(presponse) -> (Tuple[int, str]):
+        """
+        Hàm này dùng để parse một response từ API thành hai phần là comment và rating
+
+        Args:
+            presponse ([type]): response trả về vừ API
+
+        Returns:
+            Tuple[int, str]: lần lượt là rating và comment của review
+        """
         ratting = []
         comment = []
         for elem in presponse:
@@ -219,20 +247,25 @@ def getProductReviewsAPI(pproductURL: str) -> (List[Review]):
         return ratting, comment
         
     
+    '''
+      Mỗi một URL sản phẩm nếu bạn chú ý sẽ có một thành phần là `i.0284272429.2342427424` như thế này,
+    nó dùng để định danh cho sản phẩm này và mã code đầu tiên là id của người bán sản phẩm và mã code
+    thứ hai chính là id của sản phẩm. Dưới đây ta dủng regex của python đê lấy hai mã code này.
+    '''
     identifier: str  = re.search("i\.\d+.\d+", pproductURL).group(0)
     _, shop_id, product_id = identifier.split('.')
     product_reviews = []
 
-    no_reviews = 0
+    no_reviews = 0 # đây là trang navigation cần lấy, bắt đầu từ 0
     while True:
-        res = collect_comment(no_reviews*6, product_id, shop_id)
-        rattings, comments = parse_comment(res)
+        res = collect_comment(no_reviews*6, product_id, shop_id) # trả về response
+        rattings, comments = parse_comment(res) # parse nó thành comment và rating
         no_reviews += 1
         
-        if not ''.join(comments): break
+        if not ''.join(comments): break # nếu như comment trả về toàn là empty thì đã hết data để crawl và API đang trả về rác
         
-        for comment, ratting in zip(comments, rattings):
-            if not comment: continue
+        for comment, ratting in zip(comments, rattings): # bỏ vào kết quả tra về
+            if not comment: continue # đụng đến comment rỗng thì ngừng
             
             product_reviews.append(Review(comment, ratting))
             
